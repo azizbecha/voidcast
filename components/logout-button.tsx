252141e340b196d5a03de71c3d9cad1ecfb9c28d
Aziz Becha <aziz07becha@gmail.com>
2024-07-21 16:45:45 +0100

    added missing component

---
+++ components/logout-button.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { createClient } from "@/utils/supabase/client";
+import { useRouter } from "next/navigation";
+import { Button } from "./Button";
+
+export default function LogoutButton() {
+  const supabase = createClient();
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+    router.refresh();
+  };
+
+  return <Button onClick={handleLogout}>Logout</Button>;
+}
