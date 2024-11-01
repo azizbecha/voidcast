6f2a9e425dab74099b9aefbd56b425d13f776984
Aziz Becha <aziz07becha@gmail.com>
2024-11-01 21:18:08 +0100

    fixed navbar UserItems style

--- components/ui/Navbar/UserItems.tsx
+++ components/ui/Navbar/UserItems.tsx
@@ -26,26 +26,36 @@ export default function UserMenu({ user }: Props) {
 
     return (
         <Menu as="div" className="relative">
-            <MenuButton className="flex rounded-full">
-                <Image
-                    src={user?.user_metadata.avatar_url}
-                    alt={`Avatar of ${user?.user_metadata.full_name}`}
-                    width={40}
-                    height={40}
-                    className="h-6 w-6 rounded-full"
-                />
-            </MenuButton>
-            <MenuItems className="absolute right-0 mt-3 w-56 bg-primary-700 rounded-lg shadow-lg">
-                <div className="px-4 py-2 text-white font-medium">Signed in as {user?.user_metadata.full_name}</div>
-                {menuItems.map((item, index) => (
-                    <MenuItem key={index}>
-                        <Link href={item.href} className="flex items-center px-4 py-2 gap-2 text-white">
+            <div>
+                <MenuButton className="relative flex rounded-full text-sm focus:none">
+                    <Image
+                        src={user?.user_metadata.avatar_url}
+                        className="h-6 w-6 rounded-full"
+                        alt={`Image of  ${user?.user_metadata.full_name}`}
+                        width={40}
+                        height={40}
+                    />
+                </MenuButton>
+            </div>
+            <MenuItems className="absolute right-0 z-50 mt-3 w-56 origin-top-right rounded-lg bg-primary-700 shadow-lg">
+                <div className="text-white px-4 py-2 font-medium">Signed as {user?.user_metadata.full_name}</div>
+                <hr />
+                {menuItems.map((item, key) => (
+                    <MenuItem key={key}>
+                        <Link
+                            href={item.href}
+                            className={`hover:bg-primary-600 flex items-center gap-2 px-4 py-2 transition text-base font-medium text-white`}
+                        >
                             <item.icon /> {item.label}
                         </Link>
                     </MenuItem>
                 ))}
+                <hr />
                 <MenuItem>
-                    <span onClick={logout} className="flex items-center px-4 py-2 gap-2 text-white cursor-pointer">
+                    <span
+                        onClick={logout}
+                        className={`w-full bg-primary-600 hover:bg-accent cursor-pointer rounded-b-lg flex justify-start items-center gap-2 px-4 py-2 transition text-base text-start font-medium text-white`}
+                    >
                         <FaSignOutAlt /> Sign out
                     </span>
                 </MenuItem>
