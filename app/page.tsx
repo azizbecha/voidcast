361d6184f19150a1579988a63d18ad73d7438447
Aziz Becha <aziz07becha@gmail.com>
2025-06-07 23:21:30 +0100

    added Switch component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import { Tag } from "@/components/ui/Tag";
+"use client";
+
+import { useState } from "react";
+import { Switch } from "@/components/ui/Switch";
 
 export default function Home() {
+  const [checked, setChecked] = useState(true);
+
   return (
-    <div className="min-h-screen">
-      <Tag>Label</Tag>
+    <div className="min-h-screen flex items-center justify-center">
+      <Switch checked={checked} onClick={() => setChecked((c) => !c)} />
     </div>
   );
 }
---
+++ components/ui/Switch.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import * as React from "react";
+import * as SwitchPrimitives from "@radix-ui/react-switch";
+import { cn } from "@/lib/utils";
+
+const Switch = React.forwardRef<
+  React.ElementRef<typeof SwitchPrimitives.Root>,
+  React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root>
+>(({ className, ...props }, ref) => (
+  <SwitchPrimitives.Root
+    ref={ref}
+    className={cn(
+      // Container (track)
+      "peer inline-flex w-5.5 h-4 relative rounded-full border transition duration-[400ms] ease-in-out-hard shrink-0 cursor-pointer items-center",
+      // Border color depending on state
+      "data-[state=checked]:border-primary-100 data-[state=unchecked]:border-primary-300",
+      // Accessibility & disabled state
+      "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50",
+      className
+    )}
+    {...props}
+  >
+    <SwitchPrimitives.Thumb
+      className={cn(
+        // Thumb element
+        "pointer-events-none block h-2 w-2 rounded-full absolute top-1/2 left-1/2 transform -translate-y-1/2 transition duration-[400ms] ease-in-out-hard",
+        // Translate & background based on state
+        "data-[state=checked]:translate-x-0 data-[state=checked]:bg-primary-100",
+        "data-[state=unchecked]:-translate-x-full data-[state=unchecked]:bg-primary-300"
+      )}
+    />
+  </SwitchPrimitives.Root>
+));
+
+Switch.displayName = SwitchPrimitives.Root.displayName;
+export { Switch };
