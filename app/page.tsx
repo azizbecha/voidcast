e409900351aa5a0a12459d96bf1635f23c41d710
Aziz Becha <aziz07becha@gmail.com>
2025-06-07 23:46:01 +0100

    added Checkbox component

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,12 @@
 "use client";
 
-import { RadioGroup, RadioGroupItem } from "@/components/ui/Radio";
-import { Label } from "@/components/ui/Label";
-import { useState } from "react";
+import { Checkbox } from "@/components/ui/Checkbox";
 
 export default function Home() {
-  const [checked, setChecked] = useState(true);
-
-  const toggle = () => setChecked(c => !c);
 
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <RadioGroup defaultValue="option-one">
-        <div className="flex items-center space-x-2">
-          <RadioGroupItem onClick={toggle} checked={checked} value="option-one" id="option-one" />
-          <Label htmlFor="option-one">Option One</Label>
-        </div>
-        <div className="flex items-center space-x-2">
-          <RadioGroupItem onClick={toggle} checked={checked} value="option-two" id="option-two" />
-          <Label htmlFor="option-two">Option Two</Label>
-        </div>
-      </RadioGroup>
+      <Checkbox />
     </div>
   );
 }
