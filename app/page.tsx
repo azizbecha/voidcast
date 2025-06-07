34a227ee819f6f3acd2d950aa4ca68b54d26dcf1
Aziz Becha <aziz07becha@gmail.com>
2025-06-07 23:34:59 +0100

    added Radio component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,26 @@
 "use client";
 
+import { RadioGroup, RadioGroupItem } from "@/components/ui/Radio";
+import { Label } from "@/components/ui/Label";
 import { useState } from "react";
-import { Switch } from "@/components/ui/Switch";
 
 export default function Home() {
   const [checked, setChecked] = useState(true);
 
+  const toggle = () => setChecked(c => !c);
+
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <Switch checked={checked} onClick={() => setChecked((c) => !c)} />
+      <RadioGroup defaultValue="option-one">
+        <div className="flex items-center space-x-2">
+          <RadioGroupItem onClick={toggle} checked={checked} value="option-one" id="option-one" />
+          <Label htmlFor="option-one">Option One</Label>
+        </div>
+        <div className="flex items-center space-x-2">
+          <RadioGroupItem onClick={toggle} checked={checked} value="option-two" id="option-two" />
+          <Label htmlFor="option-two">Option Two</Label>
+        </div>
+      </RadioGroup>
     </div>
   );
 }
