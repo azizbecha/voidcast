c013f02489063361caac04ba88e418b1bac6e836
Aziz Becha <aziz07becha@gmail.com>
2025-06-08 06:51:41 +0100

    added Slider component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
 export default function Home() {
-  return (
-    <div className="min-h-screen flexx w-1/2 items-center justify-center"></div>
-  );
+  return <div className="min-h-screen flex items-center justify-center"></div>;
 }
