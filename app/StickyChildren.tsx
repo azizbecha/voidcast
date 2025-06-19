c74701abd9cb17db94c6827ca8ee3d12c327c564
Aziz Becha <aziz07becha@gmail.com>
2025-06-19 07:26:08 +0100

    replaced router.push with Link for best practice

--- app/StickyChildren.tsx
+++ app/StickyChildren.tsx
@@ -1,17 +1,18 @@
 "use client";
 
+import Link from "next/link";
 import { useTranslation } from "react-i18next";
 import { Button } from "@/components/ui/Button";
-import { useRouter } from "next/navigation";
 
 export const StickyChildren = () => {
-  const router = useRouter();
   const { t } = useTranslation();
 
   return (
     <div className="flex justify-between items-start mb-3">
       <h4 className="text-primary-100">{t("yourFeed")}</h4>
-      <Button onClick={() => router.push('create')}>{t("create")}</Button>
+      <Link href="create">
+        <Button>{t("create")}</Button>
+      </Link>
     </div>
   );
 };
